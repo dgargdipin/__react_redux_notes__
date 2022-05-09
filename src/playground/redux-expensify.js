f3b242f78e9c8b3024846b5ec6b95fb3a57ca1ca
Dipin Garg <dgargdipin@gmail.com>
2022-05-10 00:39:26 +0530

    remove expense

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -27,6 +27,8 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
     switch (action.type) {
         case 'ADD_EXPENSE':
             return [...state,action.expense]
+        case 'REMOVE_EXPENSE':
+            return state.filter(({id})=>id!=action.id);
         default:
             return state;
     }
@@ -68,10 +70,21 @@ const addExpense = (
         createdAt
     }
 })
-
+const removeExpense=({id})=>(
+    {
+        type:'REMOVE_EXPENSE',
+        id
+    }
+)
 store.subscribe(()=>{
     console.log(store.getState());
 })
 
-store.dispatch(addExpense({description:'Rent',amount:100}))
-store.dispatch(addExpense({description:'Coffee',amount:300}))
+const expenseOne=store.dispatch(addExpense({description:'Rent',amount:100}))
+const expenseTwo=store.dispatch(addExpense({description:'Coffee',amount:300}))
+console.log(expenseOne)
+console.log(expenseTwo)
+
+store.dispatch(removeExpense({id:expenseOne.expense.id}))
+
+

f70373817963a886698251335ed58daaf6293636
Dipin Garg <dgargdipin@gmail.com>
2022-05-10 00:28:15 +0530

    spread operator

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,48 +1,77 @@
-import {createStore,combineReducers} from 'redux'
+import { createStore, combineReducers } from 'redux'
+import {v4 as uuid} from 'uuid'
 
-const demoState={
-    expenses:[{
-        id:'asdasd',
-        description:'Rent',
-        note:'Final payment',
-        amount:54500,
-        createdAt:0
+
+
+
+const demoState = {
+    expenses: [{
+        id: 'asdasd',
+        description: 'Rent',
+        note: 'Final payment',
+        amount: 54500,
+        createdAt: 0
     }],
-    filters:{
-        text:'rent',
-        sortBy:'amount',//date or amount
-        startDate:undefined,
-        endDate:undefined,
+    filters: {
+        text: 'rent',
+        sortBy: 'amount',//date or amount
+        startDate: undefined,
+        endDate: undefined,
     }
 }
 
 
 //Expenses reducer
-const expensesReducerDefaultState=[]
-const expensesReducer=(state=expensesReducerDefaultState,action)=>{
-    switch(action.type){
+const expensesReducerDefaultState = []
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+    switch (action.type) {
+        case 'ADD_EXPENSE':
+            return [...state,action.expense]
         default:
             return state;
     }
 }
-const filtersReducerDefaultState={
-    text:'',
-    sortBy:'date',
-    startDate:undefined,
-    endDate:undefined
+const filtersReducerDefaultState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
 }
-const filtersReducer=(state=filtersReducerDefaultState,action)=>{
-    switch(action.type){
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+    switch (action.type) {
         default:
             return state;
     }
 }
 
-const store=createStore(
+const store = createStore(
     combineReducers({
-        expenses:expensesReducer,
-        filters:filtersReducer
+        expenses: expensesReducer,
+        filters: filtersReducer
     })
 );
 
-console.log(store.getState())
+const addExpense = (
+    {
+        description = '',
+        note = '', 
+        amount = 0, 
+        createdAt = 0 
+    } = {}
+) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    }
+})
+
+store.subscribe(()=>{
+    console.log(store.getState());
+})
+
+store.dispatch(addExpense({description:'Rent',amount:100}))
+store.dispatch(addExpense({description:'Coffee',amount:300}))

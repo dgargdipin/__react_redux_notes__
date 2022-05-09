f45581193a5975359053186b7dedea18637d6989
Dipin Garg <dgargdipin@gmail.com>
2022-05-09 14:57:48 +0530

    react-router

--- src/app.js
+++ src/app.js
@@ -1,5 +1,51 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
-ReactDOM.render(<p>Boilerplate</p>, document.getElementById('app'));
+
+const ExpenseDashboardPage=()=>(
+    <div>
+        This is from my dashboard component
+    </div>
+)
+const AddExpensePage=()=>(
+    <div>
+        This is from my add expense component
+    </div>
+)
+const EditExpensePage=()=>(
+    <div>
+        This is from my edit expense component
+    </div>
+)
+const HelpExpensePage=()=>(
+    <div>
+        This is from my help expense component
+    </div>
+)
+const NotFoundPage = () => (
+    <div>
+        404!
+    </div>
+)
+const routes=(
+    <BrowserRouter>
+    <Switch>
+            <Route path="/" component={ExpenseDashboardPage} exact={true}></Route>
+            <Route path="/create" component={AddExpensePage}></Route>
+            <Route path="/edit" component={EditExpensePage}></Route>
+            <Route path="/help" component={HelpExpensePage}></Route>
+            <Route component={NotFoundPage}></Route>
+    </Switch>
+    </BrowserRouter>
+)
+
+
+const root = ReactDOM.createRoot(
+    document.getElementById("app")
+);
+
+root.render(
+    routes
+);
--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ module.exports={
     devServer:{
         static:{
             directory: path.join(__dirname, 'public')
-        }
+        },
+        historyApiFallback: true
     }
 }
